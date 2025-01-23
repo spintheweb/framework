fd3bf67f7985e9b0b4f304212d45ceba0d6c4347
Giancarlo Trevisan <g.trevisan@keyvisions.it>
2025-01-23 05:13:17 +0100

    Rename

--- stwElements/stwContent.ts
+++ stwElements/stwContent.ts
@@ -8,7 +8,7 @@
 import { ISTWRecords, STWDatasources } from "../stwDatasources.ts";
 import { STWSession } from "../stwSession.ts";
 import { STWLocalized, ISTWElement, STWElement } from "./stwElement.ts";
-import { STWLayout } from "../stwContents/_wbll.ts";
+import { STWLayout } from "../stwContents/wbll.ts";
 
 /**
  * The contents that use this interface are: {@linkcode STWMenu}, {@linkcode STWNavigation}, {@linkcode STWTabs} and {@linkcode STWImagemap}
